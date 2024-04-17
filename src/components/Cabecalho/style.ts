ba1ed689ae2ae9f59bc3242af90378d55ff32052
Helen <helendianaf@gmail.com>
2024-04-17 11:25:39 -0300

    uptdate

---
+++ src/components/Cabecalho/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const HeaderT = styled.h1`
+background-color: ${(props) => props.theme.corPrincipal};
+  color:#f9f5e7;
+  text-align: center;
+  padding: 24px 0;
+}
+
+
+`
