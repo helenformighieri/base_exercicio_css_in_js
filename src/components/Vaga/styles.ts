b2caabf5d686f3cbaa12c87f8822db413f4f6673
Helen <helendianaf@gmail.com>
2024-04-22 08:03:58 -0300

    alterações no header e no vagas

--- src/components/Vaga/styles.ts
+++ src/components/Vaga/styles.ts
@@ -7,7 +7,13 @@ export const LiVaga = styled.li`
   padding: 16px;
   transition: all ease 0.3s;
   border-radius: 8px;
+
+  &:hover {
+    background-color: ${(props) => props.theme.corPrincipal};
+    color: ${(props) => props.theme.corSecundaria};
+  }
 `
+
 export const H3Vaga = styled.h3`
   font-weight: bold;
   margin-bottom: 16px;
@@ -24,6 +30,11 @@ export const LinkVaga = styled.a`
   font-size: 14px;
   border-radius: 8px;
   text-align: center;
+
+  &:hover {
+    background-color: ${(props) => props.theme.corSecundaria};
+    color: ${(props) => props.theme.corPrincipal};
+  }
 
   @media (max-width: 768px) {
     display: block;
